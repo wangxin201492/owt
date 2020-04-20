12209e3922589aa0c28c6eaf514d3b3defd39e97
wangxin201492 <wangxin201492@sina.com>
2020-04-20 15:07:44 +0800

    up

--- owt.py
+++ owt.py
@@ -1,8 +1,14 @@
+#!/usr/bin/env python
+#coding=utf8
+
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f','--foo')
 
-parser.add_argument('bar')
+parser.add_argument('bar1', choices=['a', 'b'])
+parser.add_argument('bar2')
+parser.add_argument('bar', nargs=argparse.REMAINDER)
+parser.add_argument('-t','--text')
 args = parser.parse_args()
 print args
